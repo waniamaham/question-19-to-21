484cdad47346190bd4b9764b34d25b11ab2b9eaa
WaniaM <161082325+waniamaham@users.noreply.github.com>
2024-06-02 03:35:04 +0500

    question 19 to 21
    i'm complete my question  19 to 21

---
+++ q19_to_21.ts
@@ -0,0 +1,23 @@
+// Question NO 19:Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+//  print a message indicating the number of people you are inviting to dinner.
+
+let guest_name =["Alina" ,"Sadqa" , "Noor" , "Zoha" , "Yousuf"];
+console.log(`I'm Inviting ${guest_name.length} people for dinner `);
+
+// Question no 20:Think of something you could store in a array. For example, you could
+//  make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let fvtplace =["South Korea" , "Saudi Arabia" , "Germany" ,"USA" ,"Palestine"];
+console.log("Favorite places I want to visit " ,fvtplace);
+
+// Question no 21:They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let myCar ={
+    model:2024,
+    colour:"black",
+    name:"corola XLI",
+    brand:"toyota"
+}
+console.log(`i'm buying a new car which name is ${myCar.name}  my car is new model and the  model is ${myCar.model} 
+car colour is ${myCar.colour} and car brand is ${myCar.brand}.`);
+
